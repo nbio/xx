61dbd8848cab7cee9f04a3ec3d502558e98f55ef
Randy Reddig <ydnar@shaderlab.com>
2015-07-05 13:20:55 -0700

    Split Context into own file.

--- scanner.go
+++ scanner.go
@@ -3,46 +3,8 @@ package xx
 import (
 	"encoding/xml"
 	"errors"
-	"strconv"
 	"strings"
 )
-
-// Context holds XML scanning context.
-type Context struct {
-	Decoder      *xml.Decoder
-	Value        interface{}
-	StartElement xml.StartElement
-	CharData     xml.CharData
-}
-
-// Attr returns the string value for the XML attributed named space:local.
-// Pass an empty string in space to match any namespace.
-func (ctx *Context) Attr(space, local string) string {
-	for _, a := range ctx.StartElement.Attr {
-		if (space == "" || a.Name.Space == space) && a.Name.Local == local {
-			return a.Value
-		}
-	}
-	return ""
-}
-
-// AttrBool returns a boolean value for the XML attributed named space:local.
-// Pass an empty string in space to match any namespace.
-func (ctx *Context) AttrBool(space, local string) bool {
-	v := ctx.Attr(space, local)
-	if len(v) == 0 || v == "0" || v[0] == 'f' || v[0] == 'F' {
-		return false
-	}
-	return true
-}
-
-// AttrInt returns a integer value for the XML attributed named space:local.
-// Pass an empty string in space to match any namespace.
-func (ctx *Context) AttrInt(space, local string) int {
-	v := ctx.Attr(space, local)
-	n, _ := strconv.Atoi(v)
-	return n
-}
 
 // ScanFunc is a callback that accepts an xml.StartElement, an
 // xml.CharData, and an optional interface{} value for private use.
--- scanner_test.go
+++ scanner_test.go
@@ -47,39 +47,6 @@ func TestScannerInvalidXML(t *testing.T) {
 	st.Expect(t, ok, true)
 }
 
-func TestContextAttr(t *testing.T) {
-	ctx := Context{
-		StartElement: xml.StartElement{
-			Name: xml.Name{"urn:ietf:params:xml:ns:epp-1.0", "example"},
-			Attr: []xml.Attr{
-				{xml.Name{"urn:ietf:params:xml:ns:epp-1.0", "avail"}, "1"},
-				{xml.Name{"", "alpha"}, "TRUE"},
-				{xml.Name{"", "beta"}, "1"},
-				{xml.Name{"other", "gamma"}, "false"},
-				{xml.Name{"other", "delta"}, "FALSE"},
-				{xml.Name{"", "omega"}, "hammertime"},
-				{xml.Name{"", "number"}, "42"},
-			},
-		},
-	}
-
-	st.Expect(t, ctx.Attr("", "avail"), "1")
-	st.Expect(t, ctx.AttrBool("", "avail"), true)
-	st.Expect(t, ctx.AttrInt("", "avail"), 1)
-	st.Expect(t, ctx.Attr("other", "avail"), "")
-	st.Expect(t, ctx.AttrBool("other", "avail"), false)
-	st.Expect(t, ctx.AttrInt("other", "avail"), 0)
-	st.Expect(t, ctx.AttrBool("", "alpha"), true)
-	st.Expect(t, ctx.AttrBool("", "beta"), true)
-	st.Expect(t, ctx.AttrBool("", "gamma"), false)
-	st.Expect(t, ctx.AttrBool("", "delta"), false)
-	st.Expect(t, ctx.Attr("other", "omega"), "")
-	st.Expect(t, ctx.AttrBool("other", "omega"), false)
-	st.Expect(t, ctx.Attr("", "omega"), "hammertime")
-	st.Expect(t, ctx.AttrBool("", "omega"), true)
-	st.Expect(t, ctx.AttrInt("", "number"), 42)
-}
-
 func ExampleScanner_Scan() {
 	type Part struct {
 		Name string
